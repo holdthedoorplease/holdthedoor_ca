78a2b33a490c00435694b5ff010e5a9f182c7c30
Henry Wu <holdthedoor.ca@gmail.com>
2024-11-14 09:58:15 -0800

    Update google_analytics.js

--- source/_static/google_analytics.js
+++ source/_static/google_analytics.js
@@ -2,11 +2,11 @@
 (function() {
     var gtagScript = document.createElement('script');
     gtagScript.async = true;
-    gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-CHFV056YMN';
+    gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-MK1TYYQFJE';
     document.head.appendChild(gtagScript);
   
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-    gtag('config', 'G-CHFV056YMN');
+    gtag('config', 'G-MK1TYYQFJE');
   })();
